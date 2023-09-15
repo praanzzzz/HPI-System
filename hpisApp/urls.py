50404ee15711b5b003daffd80580a9a568ed1db9
Franz Anthony Gabijan <89113736+praanzzzz@users.noreply.github.com>
2023-09-15 09:38:34 +0800

    changed to sidebar
    changed to sidebar instead of navbar

--- hpisApp/urls.py
+++ hpisApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.dashboard, name='dashboard'),
     path('patientReg/', views.patientReg, name='patientReg'),
     path('medHis/', views.medHis, name='medHis'),
     path('doc/', views.doc, name='doc'),
